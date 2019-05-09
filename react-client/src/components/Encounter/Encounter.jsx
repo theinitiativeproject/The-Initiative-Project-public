0320b45c326340da300b7a6a9be459acc628dac6
charles-spinosa <charles.spinosa@gmail.com>
2019-05-09 14:04:36 -0700

    [ feat ] add actor to active encounter

--- react-client/src/components/Encounter/Encounter.jsx
+++ react-client/src/components/Encounter/Encounter.jsx
@@ -3,26 +3,25 @@ import ActiveCharacterList from '../ActiveCharacters/ActiveCharacterList.jsx';
 import './Encounter.css';
 
 class Encounter extends React.Component {
-	constructor(props) {
-			super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		return(
-				<div className='encounterWrapper'>
-					<div className='encounterWrapperHeader'>
-						<h1>Main Encounter</h1>
-					</div>
-					<ActiveCharacterList
-						onDragEnd={this.props.onDragEnd} 
-						encounter={this.props.encounters[this.props.activeEncounter]} 
-						partyMembers={this.props.partyMembers} 
-						addToEncounters={this.props.addToEncounters} 
-					/>
-				</div>
-		);
-	}
-
+  render() {
+    return (
+      <div className="encounterWrapper">
+        <div className="encounterWrapperHeader">
+          <h1>Main Encounter</h1>
+        </div>
+        <ActiveCharacterList
+          onDragEnd={this.props.onDragEnd}
+          encounter={this.props.encounters[this.props.activeEncounter]}
+          partyMembers={this.props.partyMembers}
+          addActorToEncounter={this.props.addActorToEncounter}
+        />
+      </div>
+    );
+  }
 }
 
 export default Encounter;
