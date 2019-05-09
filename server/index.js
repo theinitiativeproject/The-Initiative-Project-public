fcc26ac0e5e399ad0e8fc133a9ce21acb46fb41d
Jeff Jardine <47231425+3Jardine@users.noreply.github.com>
2019-05-09 14:26:45 -0700

    removed redundant files

--- server/index.js
+++ server/index.js
@@ -1,19 +1,8 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
-
-// app.get('/items', function(req, res) {
-//   items.selectAll(function(err, data) {
-//     if (err) {
-//       res.sendStatus(500);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
