a4163aa147e24e32ff8d83209498e48fe72a2254
Jeff Jardine <47231425+3Jardine@users.noreply.github.com>
2019-05-10 15:41:30 -0700

    Update PartyMemberItem.jsx

--- react-client/src/components/PartyMembers/PartyMemberItem/PartyMemberItem.jsx
+++ react-client/src/components/PartyMembers/PartyMemberItem/PartyMemberItem.jsx
@@ -5,19 +5,64 @@ import { Draggable } from 'react-beautiful-dnd';
 class PartyMemberItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inEncounter: false
+    };
   }
 
   render() {
-		return(
-			<div className={ "party-member-item-wrapper " + (this.props.character.npc ? "character-npc" : "")}>
-				<span className="party-member-add"><img width="25" height="25" src="https://s3.amazonaws.com/the-initiative-project/left-arrow.svg"/></span>
-				<span className="party-member-name">{this.props.character.name}</span>
-				<span className="party-member-description-wrapper">
-					{ this.props.character.currentHP && <span className="party-member-description-hit-point"><img width="20" height="20" src="https://s3.amazonaws.com/the-initiative-project/little-health.svg"/>: {this.props.character.currentHP} / {this.props.character.maxHP}</span>}
-					{ this.props.character.armorClass && <span className="party-member-description-armour"><img width="20" height="20" src="https://s3.amazonaws.com/the-initiative-project/dark-shield.svg"/>: {this.props.character.armorClass}</span>}
-				</span>
-			</div>
-		);
+    return (
+      <div
+        className={
+          'party-member-item-wrapper ' +
+          (this.props.character.npc ? 'character-npc' : '')
+        }
+      >
+        <span className="party-member-add">
+          {!this.state.inEncounter && (
+            <img
+              width="25"
+              height="25"
+              src="https://s3.amazonaws.com/the-initiative-project/left-arrow.svg"
+              onClick={() => {
+                this.props.addActorToEncounter(this.props.character);
+                this.setState({ inEncounter: true });
+              }}
+            />
+          )}
+          {this.state.inEncounter && (
+            <img
+              width="25"
+              height="25"
+              src="https://s3.amazonaws.com/the-initiative-project/left-arrow.svg"
+            />
+          )}
+        </span>
+        <span className="party-member-name">{this.props.character.name}</span>
+        <span className="party-member-description-wrapper">
+          {this.props.character.currentHP && (
+            <span className="party-member-description-hit-point">
+              <img
+                width="20"
+                height="20"
+                src="https://s3.amazonaws.com/the-initiative-project/little-health.svg"
+              />
+              : {this.props.character.currentHP} / {this.props.character.maxHP}
+            </span>
+          )}
+          {this.props.character.armorClass && (
+            <span className="party-member-description-armour">
+              <img
+                width="20"
+                height="20"
+                src="https://s3.amazonaws.com/the-initiative-project/dark-shield.svg"
+              />
+              : {this.props.character.armorClass}
+            </span>
+          )}
+        </span>
+      </div>
+    );
   }
 }
 
